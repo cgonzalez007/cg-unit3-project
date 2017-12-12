5c514a746433aa30d6a7e696b47912c45d60a232
Chris Gonzalez <cgonzalez15@my.wctc.edu>
2017-12-12 12:36:05 -0600

    Commit #3:
    -Added login/signup page
    -only authorized users have access to certain content

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -10,6 +10,94 @@ app.controller("homeCtrl", function ($scope, $firebaseAuth, $firebaseObject) {
     app.initFirebase();
 });
 
+app.controller("followingCtrl", function ($scope, $firebaseAuth, $firebaseObject) {
+    app.initFirebase();
+});
+
+app.controller("topPostsCtrl", function ($scope, $firebaseAuth, $firebaseObject) {
+    app.initFirebase();
+});
+
+app.controller("topPostsCtrl", function ($scope, $firebaseAuth, $firebaseObject) {
+    app.initFirebase();
+});
+
+app.controller("profileCtrl", function ($scope, $firebaseAuth, $firebaseObject) {
+    app.initFirebase();
+});
+
+app.controller("authCtrl", function ($scope, $firebaseAuth, $firebaseObject) {
+    //initialize firebase
+    app.initFirebase();
+    
+    //create auth obj
+    $scope.authObj = $firebaseAuth();
+    
+    // store logeed in user
+    $scope.authUser = null;
+    $scope.user = null;
+    
+    // called on login and logout and page load
+    $scope.authObj.$onAuthStateChanged(function(firebaseUser){
+        if(firebaseUser){
+            $scope.authUser = firebaseUser;
+            $scope.user = $firebaseObject(app.firebaseRef.child('users').child($scope.authUser.uid));
+        }else{
+            $scope.authUser = null;
+            $scope.user = null;
+        }
+    });    
+
+    $scope.register = function () {
+        // get values from form
+
+        // validate
+
+        // pass values to this method
+
+        $scope.authObj.$createUserWithEmailAndPassword("my@email.com", "mypassword")
+                .then(function (firebaseUser) {
+                    console.log("User " + firebaseUser.uid + " created successfully!");
+                }).catch(function (error) {
+            console.error("Error: ", error);
+        });
+
+    };
+
+    $scope.login = function () {
+        // get values from form
+
+        // validate
+
+        // pass values to this method
+
+        $scope.authObj.$signInWithEmailAndPassword("my@email.com", "mypassword").then(function (firebaseUser) {
+            console.log("Signed in as:", firebaseUser.uid);
+        }).catch(function (error) {
+            console.error("Authentication failed:", error);
+        });
+    };
+
+    $scope.update = function () {
+        // get values from the form
+        
+        // validate
+        
+        // create object of data to update
+        var data = {firstname: 'Chris', lastname: 'Gonzalez'};
+        
+        app.firebaseRef.child('users').child($scope.authUser.uid).update(data);
+        // same as
+//        app.firebaseRef.child('users/' + $scope.authUser.uid).update(data);
+        console.log("updated");
+    };
+
+    $scope.logout = function () {
+        $scope.authObj.$signOut();
+    };
+
+}); //end controller
+
 /**
  * Create a connection to the database
  * @returns {undefined}
