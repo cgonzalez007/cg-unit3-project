15d040a6cc9de16e7c27309e6c05aa76210d7228
Chris Gonzalez <cgonzalez15@my.wctc.edu>
2017-12-12 10:52:02 -0600

    Commit #2: - From what I can see, I got Bootstrap and AngularJS working together rather nicely with any sort of errors. -Navbar looking good -Some styling defined

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,18 +1,34 @@
-var app = angular.module("cg-unit3-project",["firebase"]);
+var app = angular.module("cg-unit3-project", ["firebase", "ngAnimate", "ui.bootstrap"]);
+/**
+ * routing configurations
+ */
+
+/**
+ * Home Controlle
+ */
+app.controller("homeCtrl", function ($scope, $firebaseAuth, $firebaseObject) {
+    app.initFirebase();
+});
 
 /**
  * Create a connection to the database
  * @returns {undefined}
  */
-app.initFirebase = function (){
-    // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBjZdFe1zcdj63GMEyzCxcCKt1DrddOazo",
-    authDomain: "cg-unit3-project.firebaseapp.com",
-    databaseURL: "https://cg-unit3-project.firebaseio.com",
-    projectId: "cg-unit3-project",
-    storageBucket: "",
-    messagingSenderId: "355893287161"
-  };
-  firebase.initializeApp(config);
+app.initFirebase = function () {
+    if (!app.firebaseRef) {
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyBjZdFe1zcdj63GMEyzCxcCKt1DrddOazo",
+            authDomain: "cg-unit3-project.firebaseapp.com",
+            databaseURL: "https://cg-unit3-project.firebaseio.com",
+            projectId: "cg-unit3-project",
+            storageBucket: "",
+            messagingSenderId: "355893287161"
+        };
+        firebase.initializeApp(config);
+
+// create reference to database
+        app.firebaseRef = firebase.database().ref('/');
+    }   
+
 };
